fdb7abadddfba82690ff7d90a490057acb5be138
Ruma303 <matteoh8@hotmail.it>
2025-10-29 17:38:18 +0100

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { DatePipe } from '@angular/common';
 
 import { AppComponent } from './app';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks';
-import { TaskComponent } from './tasks/task/task';
-import { NewTaskComponent } from './tasks/new-task/new-task';
-import { CardComponent } from './shared/card/card';
+import { SharedModule } from './shared/shared.module';
+import { TaskModule } from './tasks/task.module';
 
 @NgModule({
-  declarations: [ // Non standalone components
+  declarations: [
     AppComponent,
     HeaderComponent,
     UserComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent,
-    CardComponent
   ],
-  imports: [// Standalone components ed import di altri moduli Angular
+  imports: [
     BrowserModule,
-
-    // Queste dipendenze non sono necessarie,
-    // in quanto importate già da BrowserModule
-    FormsModule,
-    DatePipe
+    SharedModule,
+    TaskModule
   ],
   bootstrap: [AppComponent],
   providers: [],
 })
-export class AppModule {
-
-}
+export class AppModule {}
